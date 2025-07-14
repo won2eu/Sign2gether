e8c0e4a201379440252a17728608309deb88da16
won2eu <hasw3314@gmail.com>
2025-07-14 17:24:41 +0900

    minify 끄기

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -5,6 +5,25 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, //eslint 관련 에러 무시하자~
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      if (config.optimization && Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer.forEach((minimizer: any) => {
+          if (
+            minimizer.constructor.name === 'TerserPlugin' &&
+            minimizer.options
+          ) {
+            if (Array.isArray(minimizer.options.exclude)) {
+              minimizer.options.exclude.push(/react-signature-canvas/);
+            } else {
+              minimizer.options.exclude = [/react-signature-canvas/];
+            }
+          }
+        });
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
