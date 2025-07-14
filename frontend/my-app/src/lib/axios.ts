f164ea380241f50f4b69632319229fb837d33c51
won2eu <hasw3314@gmail.com>
2025-07-14 16:17:05 +0900

    백엔드 배포 url로 변경(2)

--- frontend/my-app/src/lib/axios.ts
+++ frontend/my-app/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://sign2gether-api-production.up.railway.app',
   withCredentials: true, // 쿠키 포함
 })
 
