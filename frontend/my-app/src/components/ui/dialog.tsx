b4d72c6f1e26e4c28b45ba5136d4c1dd9fb7d6dd
won2eu <hasw3314@gmail.com>
2025-07-15 21:24:48 +0900

    투명도 제거

--- frontend/my-app/src/components/ui/dialog.tsx
+++ frontend/my-app/src/components/ui/dialog.tsx
@@ -60,7 +60,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
-          "bg-black-100 text-white data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[26%] left-[85%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-40%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
+          "bg-black text-white data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[26%] left-[85%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-40%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
           className
         )}
         {...props}
